ea0f32479f699016f9c083e699f360354b21d0aa
saskiaburmy <saskiaburrmeyy@gmail.com>
2026-03-15 15:18:45 +0700

    initial commit

---
+++ portofolio.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function portofolio() {
+  return (
+    <div>
+      <h1>portofolio Saskia Burmaynisa x4 </h1>
+      <p>hello,ini adalah portofolio pertama saya. </p>
+    </div>
+  )
+}
+
+export default portofolio
